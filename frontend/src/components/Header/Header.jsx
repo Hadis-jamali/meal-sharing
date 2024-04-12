6e0b673e343e3dbb53b50aeda7c61bf9a6179726
Hadis Jamali <hadisjamali95@gmail.com>
2024-04-12 14:27:45 +0200

    add navbar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,37 +1,14 @@
 import React from "react";
 import "./Header.css";
+import NavBar from "../NavBar";
 function Header() {
   return (
     <div class="container">
-      <div className="container-navbar">
-        <div className="navbar-div">
-          <a href="#section1" className="logo">
-            MealSharing
-          </a>
-          <nav>
-            <ul className="nav-list">
-              <li className="nav-hover">
-                <a href="#section1" className="navbarLink">
-                  Home
-                </a>
-              </li>
-              <li className="nav-hover">
-                <a className="navbarLink"> Menu </a>
-              </li>
-              <li className="nav-hover">
-                <a className="navbarLink"> Foods </a>
-              </li>
-              <li className="nav-hover">
-                <a className="navbarLink"> Contact </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
+      <NavBar />
       <div data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-          <h1 class="head-title">Order your </h1>
-          <h1 class="head-desc">favorite food here</h1>
-        </div>
+        <h1 class="head-title">Order your </h1>
+        <h1 class="head-desc">favorite food here</h1>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="container-navbar">
+      <div className="navbar-div">
+        <a href="#section1" className="logo">
+          MealSharing
+        </a>
+        <nav>
+          <ul className="nav-list">
+            <li className="nav-hover">
+              <a href="#section1" className="navbarLink">
+                Home
+              </a>
+            </li>
+            <li className="nav-hover">
+              <a className="navbarLink"> Menu </a>
+            </li>
+            <li className="nav-hover">
+              <a className="navbarLink"> Foods </a>
+            </li>
+            <li className="nav-hover">
+              <a className="navbarLink"> Contact </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
