a89f78944ab966a0d0877cbc0db3f36220c20887
Hadis Jamali <hadisjamali95@gmail.com>
2024-04-24 10:43:08 +0200

    add contact us

---
+++ frontend/src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./ContactUs.css";
+function ContactUs() {
+  return (
+    <div className="about">
+      <div className="main-about">
+        <div className="animation"></div>
+        <div className="about-text">
+          <h1>About Us</h1>
+          <p>
+            Italian culinary tradition and Danish coziness meet here, where our chef has created a
+            menu driven by passion. With a background rich in experience and a deep love for THE
+            Italian cuisine, our chef offers a true taste of Italy. Our menu is a journey through
+            Italy's gastronomic landscape, where fresh Danish ingredients meet classic Italian
+            recipes. Come and let our chef take you on a culinary adventure. Welcome everyone!
+          </p>
+
+          <div className="social-media">
+            <div>
+              <Link to="https://www.linkedin.com/in/hadisjamali/">
+                <button type="button">Let's Talk</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactUs;
--- frontend/src/components/MenuPage/MenuPage.jsx
+++ frontend/src/components/MenuPage/MenuPage.jsx
@@ -8,7 +8,7 @@ function MenuPage() {
       </div>
       <div className="menu-container">
         <h1>Menu</h1>
-        <p>Available meals in our place</p>
+        <p className="Available">Available meals in our place</p>
         <div>
           <h3>Pizza salami</h3>
           <p>delicious italian food</p>
