48e976d1c83bc194020c97041fe6951975c2e2f4
Hadis Jamali <hadisjamali95@gmail.com>
2024-05-10 11:43:44 +0200

    responsive

--- frontend/src/components/Header/Slider.jsx
+++ frontend/src/components/Header/Slider.jsx
@@ -6,7 +6,7 @@ import "./Header.css";
 function Slider() {
   const images = [
     "https://images.unsplash.com/photo-1625943557969-5b990001e693?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1490645935967-10de6ba17061?q=80&w=2053&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1608835149345-b4d77bc490ae?q=80&w=2672&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1493770348161-369560ae357d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ];
   return (
