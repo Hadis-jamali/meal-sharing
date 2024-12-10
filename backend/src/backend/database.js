0204a6877b19df069284c7eda577115dec87df13
Hadis Jamali <hadisjamali95@gmail.com>
2024-12-10 18:44:40 +0100

    change .env

--- backend/src/backend/database.js
+++ backend/src/backend/database.js
@@ -6,7 +6,7 @@ const knex = require("knex")({
     host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || 'Hm4310913229',
+    password: process.env.DB_PASSWORD || 'hadis123',
     database: process.env.DB_NAME || 'mealsharing',
   },
   pool: { min: 0, max: 7 },
